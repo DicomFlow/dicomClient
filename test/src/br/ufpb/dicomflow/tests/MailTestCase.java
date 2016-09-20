575de4716fc5204eeee83dbf2ce3c24ed3390f2e
netolucena <juracylucena@gmail.com>
2016-09-19 23:10:43 -0300

    Add email

--- test/src/br/ufpb/dicomflow/tests/MailTestCase.java
+++ test/src/br/ufpb/dicomflow/tests/MailTestCase.java
@@ -1,10 +1,13 @@
 package br.ufpb.dicomflow.tests;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Properties;
 
 import org.junit.Test;
 
+import br.ufpb.dicomflow.integrationAPI.conf.IntegrationAPIProperties;
+import br.ufpb.dicomflow.integrationAPI.mail.FilterIF;
 import br.ufpb.dicomflow.integrationAPI.mail.MailAuthenticatorIF;
 import br.ufpb.dicomflow.integrationAPI.mail.MailMessageReaderIF;
 import br.ufpb.dicomflow.integrationAPI.mail.MailServiceExtractorIF;
@@ -18,7 +21,15 @@ import br.ufpb.dicomflow.integrationAPI.message.xml.ServiceIF;
 public class MailTestCase {
 	
 	@Test
-	public void testReceive() {
+	public void testReceive() {	
+		
+//		Properties pop3Props = new Properties();
+//		 
+//		pop3Props.setProperty("mail.pop3.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+//		pop3Props.setProperty("mail.pop3.socketFactory.fallback", "false");
+//		pop3Props.setProperty("mail.pop3.port",  "995");
+//		pop3Props.setProperty("mail.pop3.socketFactory.port", "995");
+
 		Properties props = new Properties();// System.getProperties();
 		props.put("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 		props.put("mail.imap.socketFactory.fallback", "false");
@@ -33,13 +44,44 @@ public class MailTestCase {
 		receiver.setMessageReader(smtpMesssaStrategy);
 		receiver.setServiceExtractor(serviceExtractor);
 		
-		Iterator<ServiceIF> iterator = receiver.receive(new SMTPFilter());
-		
+		List<ServiceIF> services = receiver.receive(new SMTPFilter());
+		Iterator<ServiceIF> iterator = services.iterator();
 		while (iterator.hasNext()) {
 			ServiceIF serviceIF = (ServiceIF) iterator.next();
-			System.out.println("MessageID:" +serviceIF.getMessageID() + "Name: " + serviceIF.getName() + "Action: " +serviceIF.getAction());
+			//System.out.println("MessageID:" +serviceIF.getMessageID() + "Name: " + serviceIF.getName() + "Action: " +serviceIF.getAction());
 		}
 	}
 	
-	
+	public void testReceive2() {
+		try {
+
+			Properties props = new Properties();
+			IntegrationAPIProperties.getInstance().load(IntegrationAPIProperties.CONFIG_FILE_PATH);
+			props = IntegrationAPIProperties.getInstance().getReceiveProperties();
+
+			MailAuthenticatorIF mailAuthenticator = new SMTPAuthenticator(
+					props.getProperty(IntegrationAPIProperties.AUTHENTICATION_LOGIN),
+					props.getProperty(IntegrationAPIProperties.AUTHENTICATION_PASSWORD));
+			MailServiceExtractorIF mailServiceExtractor = new SMTPServiceExtractor();
+			MailMessageReaderIF mailMessageReader = new SMTPMessageReader(
+					props.getProperty(IntegrationAPIProperties.PROVIDER_HOST),
+					props.getProperty(IntegrationAPIProperties.PROVIDER_FOLDER));
+
+			SMTPReceiver receiver = new SMTPReceiver();
+			receiver.setProperties(props);
+			receiver.setAuthenticatorBuilder(mailAuthenticator);
+			receiver.setMessageReader(mailMessageReader);
+			receiver.setServiceExtractor(mailServiceExtractor);
+
+			FilterIF filter = new SMTPFilter();
+			
+			List<ServiceIF> services = receiver.receive(filter);
+			
+			System.out.println(services.size());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
