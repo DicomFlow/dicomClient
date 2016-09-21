135bb5708b88ccccfbf81395fc4f1e6929637694
juracyneto <juracyneto@sti.ufpb.br>
2016-09-21 09:20:07 -0300

    Interface

--- src/br/ufpb/dicomflow/gui/application/MainController.java
+++ src/br/ufpb/dicomflow/gui/application/MainController.java
@@ -1,5 +1,6 @@
 package br.ufpb.dicomflow.gui.application;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -16,9 +17,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -50,6 +54,18 @@ public class MainController implements Initializable {
     private Accordion studiesAccordion;
     
     @FXML ComboBox<String> ordenarPor;
+    
+    @FXML
+    private ImageView iconeNovosExames;
+    
+    @FXML
+    private ImageView iconeExames;
+    
+    @FXML
+    private ImageView iconeLixeira;
+    
+    @FXML 
+    private Hyperlink novosExames;
             
     @FXML 
     protected void handleEntrarButtonAction(ActionEvent event) {        
@@ -65,7 +81,7 @@ public class MainController implements Initializable {
 		}
     }
     
-    private void initFields() {
+    private void initFields() {    	
     	ordenarPor = new ComboBox<String>();
     	ordenarPor.getItems().addAll("Data", "Nome");    	
     	
@@ -85,7 +101,7 @@ public class MainController implements Initializable {
      }
     
     public void showStudies(ActionEvent event) throws IOException {
-    	Scene scene = novosExamesButton.getScene();
+    	Scene scene = Main.getpStage().getScene();
         Stage stage = (Stage) scene.getWindow();
         
 
