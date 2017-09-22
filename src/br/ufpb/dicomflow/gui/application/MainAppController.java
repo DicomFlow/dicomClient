7913033accac1db4c688e3dd1e4096a5fc3223ad
netolucena <juracylucena@gmail.com>
2017-09-21 21:59:37 -0300

    projetando nova interface

--- src/br/ufpb/dicomflow/gui/application/MainAppController.java
+++ src/br/ufpb/dicomflow/gui/application/MainAppController.java
@@ -1,21 +1,50 @@
 package br.ufpb.dicomflow.gui.application;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import br.ufpb.dicomflow.gui.business.AuthenticationProcessor;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class MainAppController implements Initializable  {
+	
+	@FXML
+	private TextField pesquisarTextField;
 
+	@FXML
+	private Label contatosLabel;
+	
+	@FXML
+	private Label inboxLabel;
+	
+	@FXML
+	private Label favoritosLabel;
+	
+	@FXML
+	private Label configuracoesLabel;
+	
+	@FXML
+	ImageView contatosIcon;
+	
+	@FXML
+	private ImageView inboxIcon;
+	
+	@FXML
+	private ImageView favoritosIcon;
+	
+	@FXML
+	private ImageView configuracoesIcon;
+	
+	
 	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
+	public void initialize(URL location, ResourceBundle resources) {		
+		setIcons();
 	}
 	
 	@FXML
@@ -31,7 +60,13 @@ public class MainAppController implements Initializable  {
 	public void exitAction(ActionEvent event){
 		Platform.exit();
 		System.exit(0);
+	}	
+	
+	private void setIcons() {
+		contatosIcon.setImage(new Image(getClass().getResourceAsStream("img/employee.png")));
+		inboxIcon.setImage(new Image(getClass().getResourceAsStream("img/download.png")));
+		favoritosIcon.setImage(new Image(getClass().getResourceAsStream("img/star.png")));
+		configuracoesIcon.setImage(new Image(getClass().getResourceAsStream("img/cogwheel.png")));
 	}
-
 
 }
